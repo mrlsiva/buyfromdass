47d17489dce0d5b63c0250f11f528c025edc4d25
siva <pmrlsivas@gmail.com>
2025-10-15 11:33:02 +0530

    yeys

--- index.php
+++ index.php
@@ -17,28 +17,9 @@
 
 	<title>Buy From Dass</title>
 
-	<!-- Standard Favicon -->
-	<link rel="icon" type="image/x-icon" href="images//favicon.png" />
-
-	<!-- For iPhone 4 Retina display: -->
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
-
-	<!-- For iPad: -->
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
-
-	<!-- For iPhone: -->
-	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">
-
-	<!-- Custom - Theme CSS -->
-	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
-
-	<!--[if lt IE 9]>
-		<script src="js/html5/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
-<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+<body>
 	<?php include 'includes/header.html'; ?>
 
 
