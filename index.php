0c39a2de162064172fbaff0d677e1bfb82ad055d
siva <pmrlsivas@gmail.com>
2025-10-15 10:32:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class=""><!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Buy From Dass</title>
+
+	<!-- Standard Favicon -->
+	<link rel="icon" type="image/x-icon" href="images//favicon.png" />
+
+	<!-- For iPhone 4 Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
+
+	<!-- For iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
+
+	<!-- For iPhone: -->
+	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">
+
+	<!-- Custom - Theme CSS -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<!--[if lt IE 9]>
+		<script src="js/html5/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
+	<?php include 'includes/header.html'; ?>
+
+
+	<main class="site-main page-spacing">
+		<!-- Photo Slider -->
+		<div class="photo-slider container-fluid no-padding">
+			<!-- Main Carousel -->
+			<div id="main-carousel" class="carousel slide carousel-fade" data-ride="carousel">
+				<div role="listbox" class="carousel-inner">
+					<div class="item active">
+						<img width="1920" height="960" alt="slider" src="images/slider-1.jpg">
+						<div class="carousel-caption">
+							<img src="images/slider-ic.png" alt="slider-ic" width="131" height="129">
+							<h3>Buy From Dass</h3>
+							<img src="images/section-seprator-1.png" alt="section-seprator" width="76" height="10" />
+							<p>YOUR ONE-STOP SOLUTION FOR ALL HOME NEEDS</p>
+						</div>
+						<span class="goto-next"><a href="#order-section"><i class="fa fa-arrow-down bounce"
+									aria-hidden="true"></i>scroll down</a></span>
+					</div>
+
+					<a class="left carousel-control" href="#main-carousel" role="button" data-slide="prev">
+						<i class="fa fa-caret-left" aria-hidden="true"></i>
+					</a>
+					<a class="right carousel-control" href="#main-carousel" role="button" data-slide="next">
+						<i class="fa fa-caret-right" aria-hidden="true"></i>
+					</a>
+				</div>
+			</div><!-- Main Carousel /-  -->
+		</div><!-- Photo Slider /- -->
+		
+		<?php include 'includes/about.html'; ?>
+
+		<!-- Quotes Section -->
+		<div class="quotes-section container-fluid no-padding">
+			<div class="section-padding"></div>
+			<!-- Container -->
+			<div class="container">
+				<div class="quotes-content">
+					<h3>WE DON’T SELL PRODUCTS. WE SELL PEACE OF MIND.</h3>
+					<!-- <img src="images/quote-ic-1.png" alt="quote-ic" /> -->
+					<h5>Choosing a home appliance today is more complicated than ever. With a flood of options across
+						online platforms (Amazon, Flipkart), major retail chains (Reliance Digital, Croma), and local
+						dealers, even a small mistake can be costly.</h5>
+					<h5>That’s where Buy From Dass steps in. Not as salespeople, but as your personal, independent
+						consultants.</h5>
+					<h5>We’re not here to push products or promote brands. Our sole aim is to help you choose the right
+						product, from the right place, at the right time – whether it's a refrigerator, washing machine,
+						air conditioner, television, or any other appliance.</h5>
+					<!-- <img src="images/quote-ic-2.png" alt="quote-ic" /> -->
+				</div>
+			</div><!-- Container /- -->
+			<div class="section-padding"></div>
+		</div><!-- Quotes Section /- -->
+
+		<!-- About Section -->
+		<div class="about-section container-fluid">
+			<!-- Container -->
+			<div class="container">
+				<div class="col-md-8 col-sm-8 col-xs-6 about-content">
+					<!-- Section Header -->
+					<div class="section-header left-header">
+						<h3>HOW WE HELP</h3>
+						<h4>We offer unbiased, expert guidance for every stage of your purchase journey:</h4>
+						<img src="images/section-seprator.png" alt="section-seprator" width="76" height="10" />
+					</div><!-- Section Header /- -->
+					<div class="about-box">
+						<p>• What To Buy: Tailored recommendations based on your lifestyle, budget, usage patterns, and
+							the latest in durability and technology.</p>
+						<p>• Where To Buy: Guidance on the most reliable platforms and stores offering the best
+							combination of price, service, and after-sales support.</p>
+						<p>• When To Buy: Insider tips on when to strike the best deals — festive offers, end-of-season
+							sales, or model upgrades.</p>
+						<!-- <ul>
+							<li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" title="Google"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+						</ul>
+						<a href="#" title="View More" class="view-more">View More</a> -->
+					</div>
+					<!-- Section Header -->
+					<div class="section-header left-header">
+						<h4>We stay on top of the market, trends, and technology so that you don’t have to.</h4>
+
+					</div><!-- Section Header /- -->
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-6 about-img">
+					<img src="images/about-1.png" alt="about" width="597" height="965" />
+				</div>
+
+			</div><!-- Container /- -->
+		</div><!-- About Section /- -->
+
+		<!-- Process Section -->
+		<div class="process-section container-fluid no-padding gray-bg">
+			<div class="section-padding"></div>
+			<!--<div class="process-top-image">
+				<img src="images/process-top-img.png" alt="process-top-img" />
+			</div> -->
+			<!-- Container -->
+			<div class="container">
+				<!-- Section Header -->
+				<div class="section-header">
+					<!-- <h3>Our Baking Theory</h3> -->
+					<h5>WHY CHOOSE US?</h5>
+					<img src="images/section-seprator.png" alt="section-seprator" width="76" height="10" />
+					<!-- <p>The one day when the lady met this fellow and they knew it was much more than a hunch the first
+						mate and his Skipper too will do their comfortable.</p> -->
+				</div><!-- Section Header /- -->
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12 no-padding process-box">
+						<div class="col-md-8 col-sm-12 col-xs-12">
+							<div class="col-md-8 col-sm-8 col-xs-7">
+								<img src="images/why-choose/1.svg" alt="process" width="120px" />
+								<span></span>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-5">
+								<div class="process-content">
+									<h3>Unbiased Expertise</h3>
+									<p> No brand bias. No sales pitch. Just honest advice.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12 col-sm-12 col-xs-12 no-padding process-box">
+						<div class="col-md-8 col-sm-12 col-xs-12">
+							<div class="col-md-8 col-sm-8 col-xs-7">
+								<img src="images/why-choose/2.svg" alt="process" width="120px" />
+
+								<span></span>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-5">
+								<div class="process-content">
+									<h3>Smart Choices, First Try</h3>
+									<p>Avoid costly mistakes and get it right the first time.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12 col-sm-12 col-xs-12 no-padding process-box">
+						<div class="col-md-8 col-sm-12 col-xs-12">
+							<div class="col-md-8 col-sm-8 col-xs-7">
+								<img src="images/why-choose/3.svg" alt="process" width="120px" />
+								<span></span>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-5">
+								<div class="process-content">
+									<h3>Time Saved, Clarity Gained</h3>
+									<p>Save hours of research and confusion.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12 col-sm-12 col-xs-12 no-padding process-box">
+						<div class="col-md-8 col-sm-12 col-xs-12">
+							<div class="col-md-8 col-sm-8 col-xs-7">
+								<img src="images/why-choose/4.svg" alt="process" width="120px" />
+								<span></span>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-5">
+								<div class="process-content">
+									<h3>Decisions You Can Trust</h3>
+									<p>Make informed decisions backed by 50+ years of industry knowledge.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12 col-sm-12 col-xs-12 no-padding process-box">
+						<div class="col-md-8 col-sm-12 col-xs-12">
+							<div class="col-md-8 col-sm-8 col-xs-7">
+								<img src="images/why-choose/5.svg" alt="process" width="120px" />
+								<span></span>
+							</div>
+							<div class="col-md-4 col-sm-4 col-xs-5">
+								<div class="process-content">
+									<h3>Confidence in Every Purchase</h3>
+									<p>Enjoy peace of mind with every purchase.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div><!-- Container /- -->
+			<!-- <div class="process-bottom-image">
+				<img src="images/process-bottom-img.png" alt="process-bottom-img" />
+			</div>-->
+			<div class="section-padding"></div>
+		</div><!-- Process Section /- -->
+
+		<!-- Quotes Section -->
+		<div class="quotes-section container-fluid no-padding">
+			<div class="section-padding"></div>
+			<!-- Container -->
+			<div class="container">
+				<div class="quotes-content">
+					<h3>READY TO BUY SMARTER?</h3>
+					<!-- <img src="images/quote-ic-1.png" alt="quote-ic" /> -->
+					<h5>Whether you're setting up a new home, replacing an old appliance, or simply exploring your
+						options, we’re here to make your next purchase smooth and stress-free.</h5>
+
+					<!-- <img src="images/quote-ic-2.png" alt="quote-ic" /> -->
+				</div>
+			</div><!-- Container /- -->
+			<div class="section-padding"></div>
+		</div><!-- Quotes Section /- -->
+
+
+	
+	<?php include 'includes/cta.html'; ?>
+
+	</main>
+	
+	<?php include 'includes/footer.html'; ?>
+
+	<!-- JQuery v1.12.4 -->
+	<script src="js/jquery-1.12.4.min.js"></script>
+
+	<!-- Library - Js -->
+	<script src="libraries/lib.js"></script><!-- Bootstrap JS File v3.3.5 -->
+
+	<!-- Library - Google Map API -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDW40y4kdsjsz714OVTvrw7woVCpD8EbLE"></script>
+
+	<!-- Library - Theme JS -->
+	<script src="js/functions.js"></script>
+</body>
+
+</html>
