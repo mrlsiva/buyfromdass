53a7ebae4e030f5b1c678a7af9dea150c52f37c3
siva <pmrlsivas@gmail.com>
2025-10-15 11:25:27 +0530

    test

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
-<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
-<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
-<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
-<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
-<html class=""><!--<![endif]-->
+
+<html class="">
 
 <head>
 	<meta charset="utf-8">
@@ -17,24 +13,12 @@
 
 	<title>Buy From Dass</title>
 
-	<!-- Standard Favicon -->
-	<link rel="icon" type="image/x-icon" href="images//favicon.png" />
 
-	<!-- For iPhone 4 Retina display: -->
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
-
-	<!-- For iPad: -->
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
-
-	<!-- For iPhone: -->
-	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">
 
 	<!-- Custom - Theme CSS -->
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	<!--[if lt IE 9]>
-		<script src="js/html5/respond.min.js"></script>
-    <![endif]-->
+	
 
 </head>
 
@@ -255,15 +239,6 @@
 	
 
 
-	<!-- JQuery v1.12.4 -->
-	<script src="js/jquery-1.12.4.min.js"></script>
-
-	<!-- Library - Js -->
-	<script src="libraries/lib.js"></script><!-- Bootstrap JS File v3.3.5 -->
-
-
-	<!-- Library - Theme JS -->
-	<script src="js/functions.js"></script>
 </body>
 
 </html>
