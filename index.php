92c02bb1177198339d41d4b7256f7fe5143bcefb
siva <pmrlsivas@gmail.com>
2025-10-15 11:26:58 +0530

    laod

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
-
-<html class="">
+<!--[if lt IE 7 ]> <html class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!-->
+<html class=""><!--<![endif]-->
 
 <head>
 	<meta charset="utf-8">
@@ -13,17 +17,29 @@
 
 	<title>Buy From Dass</title>
 
+	<!-- Standard Favicon -->
+	<link rel="icon" type="image/x-icon" href="images//favicon.png" />
 
+	<!-- For iPhone 4 Retina display: -->
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images//apple-touch-icon-114x114-precomposed.png">
+
+	<!-- For iPad: -->
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images//apple-touch-icon-72x72-precomposed.png">
+
+	<!-- For iPhone: -->
+	<link rel="apple-touch-icon-precomposed" href="images//apple-touch-icon-57x57-precomposed.png">
 
 	<!-- Custom - Theme CSS -->
 	<link rel="stylesheet" type="text/css" href="style.css">
 
-	
+	<!--[if lt IE 9]>
+		<script src="js/html5/respond.min.js"></script>
+    <![endif]-->
 
 </head>
 
 <body data-offset="200" data-spy="scroll" data-target=".ow-navigation">
-
+	<?php include 'includes/header.html'; ?>
 
 
 	<main class="site-main page-spacing">
@@ -233,12 +249,21 @@
 
 
 	
-
+	<?php include 'includes/cta.html'; ?>
 
 	</main>
 	
+	<?php include 'includes/footer.html'; ?>
+
+	<!-- JQuery v1.12.4 -->
+	<script src="js/jquery-1.12.4.min.js"></script>
+
+	<!-- Library - Js -->
+	<script src="libraries/lib.js"></script><!-- Bootstrap JS File v3.3.5 -->
 
 
+	<!-- Library - Theme JS -->
+	<script src="js/functions.js"></script>
 </body>
 
 </html>
